53fc2ff1aadaa7e81be13eb78caf4f02d8e7105a
Alexander Senier <senier@componolit.com>
2019-07-19 17:32:30 +0200

    WIP

--- src/extracts.adb
+++ src/extracts.adb
@@ -1,7 +1,8 @@
 with Ada.Text_IO; use Ada.Text_IO;
 
 package body Extracts with
-   SPARK_Mode
+  SPARK_Mode,
+  Annotate => (GNATprove, Terminating)
 is
 
    procedure Lemma_Exp_Is_Monotonic (Base       : Long_Integer;
@@ -10,27 +11,12 @@ is
    with
       Pre  => Exponent_1 <= Exponent_2,
       Post => Base ** Exponent_1 <= Base ** Exponent_2,
-      Ghost;
+      Ghost, Annotate => (GNATprove, Terminating);
 
    procedure Lemma_Exp_Is_Monotonic (Base       : Long_Integer;
                                      Exponent_1 : Natural;
                                      Exponent_2 : Natural)
-   is null with SPARK_Mode => Off;
-
-   procedure Lemma_Exp_Eq (Base       : Long_Integer;
-                           Exponent_1 : Natural;
-                           Exponent_2 : Natural)
-   with
-      Pre  => Exponent_1 = Exponent_2,
-      Post => Base ** Exponent_1 = Base ** Exponent_2,
-      Ghost;
-
-   procedure Lemma_Exp_Eq (Base       : Long_Integer;
-                           Exponent_1 : Natural;
-                           Exponent_2 : Natural) is
-   begin
-      null;
-   end Lemma_Exp_Eq;
+   is null;
 
    procedure Lemma_Lt_Mult_Lt_Lt (Left        : Long_Integer;
                                   Left_Bound  : Long_Integer;
@@ -49,86 +35,121 @@ is
                                   Right       : Long_Integer;
                                   Right_Bound : Long_Integer;
                                   Result      : Long_Integer)
-   is
-   begin
-      null;
-   end Lemma_Lt_Mult_Lt_Lt;
+   is null;
+
+   procedure Lemma_Mult_Gt_0 (Factor_1 : Long_Integer;
+                              Factor_2 : Long_Integer)
+   with
+       Pre  => Factor_1 >= 0 and Factor_2 >= 0,
+       Post => Factor_1 * Factor_2 >= 0,
+       Ghost;
+
+   procedure Lemma_Mult_Gt_0 (Factor_1 : Long_Integer;
+                              Factor_2 : Long_Integer)
+   is null;
+
+   procedure Lemma_Val_Lt_Mod (Value  : Long_Integer;
+                               Modulo : Long_Integer)
+   with
+       Pre  => Modulo > 0,
+       Post => Value mod Modulo < Modulo,
+       Ghost;
+
+   procedure Lemma_Val_Lt_Mod (Value  : Long_Integer;
+                               Modulo : Long_Integer)
+   is null;
+
 
    function Extract (Data   : Array_Type;
                      Offset : Offset_Type) return Value_Type
    is
-      Element_Size   : constant Natural := Element_Type'Size;
-      Value_Size     : constant Natural := Value_Type'Size;
+      pragma Assert (Element_Type'Size < 63);
+--        pragma Assert (Data'Length > (Offset_Type'Pos (Offset) + Value_Type'Size - 1) / Element_Type'Size);
+--        pragma Assert (Element_Type'Size * Natural ((Offset_Type'Pos (Offset) + Long_Integer (Value_Type'Size) - 1) / Long_Integer (Element_Type'Size)) <= Natural'Last);
+--        pragma Assert (2 ** (Element_Type'Size * Natural ((Offset_Type'Pos (Offset) + Long_Integer (Value_Type'Size) - 1)
+--                                          / Long_Integer (Element_Type'Size)))
+--                                          * 2 ** Element_Type'Size <= Long_Integer'Last);
 
-      Right          : constant Long_Integer := Offset_Type'Pos (Offset) / Long_Integer (Element_Size);
-      Left           : constant Long_Integer := (Offset_Type'Pos (Offset) + Long_Integer (Value_Size) - 1)
-                                                / Long_Integer (Element_Size);
-      Element_Offset : constant Natural      := Offset_Type'Pos (Offset) mod Element_Size;
-      Left_Offset    : constant Natural      := Element_Size - Element_Offset;
+      Right          : constant Long_Integer := Offset_Type'Pos (Offset) / Long_Integer (Element_Type'Size);
+      Left           : constant Long_Integer := (Offset_Type'Pos (Offset) + Long_Integer (Value_Type'Size) - 1)
+                                                / Long_Integer (Element_Type'Size);
+      Element_Offset : constant Natural      := Offset_Type'Pos (Offset) mod Element_Type'Size;
+      Left_Offset    : constant Natural      := Element_Type'Size - Element_Offset;
       Result         : Long_Integer          := 0;
 
-      pragma Assert (Element_Size - Offset_Type'Pos (Offset) mod Element_Size <= 63);
+      function Element_Type_Size return Natural is (Element_Type'Size)
+        with Post => (Element_Type_Size'Result = Element_Type'Size);
+
+      pragma Assert (Element_Type'Size - Offset_Type'Pos (Offset) mod Element_Type'Size <= 63);
 
       function D (Pos : Long_Integer) return Element_Type with
          Pre => Pos >= 0 and then Index_Type'Pos (Data'First) <= Index_Type'Pos (Data'Last) - Pos;
 
       function D (Pos : Long_Integer) return Element_Type
       is
-         E : constant Natural := (Element_Offset + Value_Size + Element_Size - 1) mod Element_Size + 1;
+         E : constant Natural := (Element_Offset + Value_Type'Size + Element_Type'Size - 1) mod Element_Type'Size + 1;
       begin
-         Lemma_Exp_Is_Monotonic (2, E, Element_Size);
+         Lemma_Exp_Is_Monotonic (2, E, Element_Type'Size);
+         pragma Assert (2 ** E <= 2 ** Element_Type'Size);
          declare
-            Mask : constant Long_Integer := (if Pos < Left then 2 ** Element_Size else 2 ** E);
+            Mask : constant Long_Integer := (if Pos < Left then 2 ** Element_Type'Size else 2 ** E);
             Val  : constant Element_Type := Data (Index_Type'Val ((Index_Type'Pos (Data'Last) - Pos)));
          begin
-            pragma Assert (2 ** E <= 2 ** Element_Size);
             return Element_Type'Val (Element_Type'Pos (Val) mod Mask);
          end;
       end D;
 
-      ES  : Natural := Element_Size;
    begin
+      pragma Assert (Element_Type'Size * Natural (Left) <= Natural'Last);
+      --  pragma Assert (2 ** (Element_Type'Size * Natural (Left)) * 2 ** Element_Type'Size <= Long_Integer'Last);
+
       for I in Right .. Left - 1
       loop
          Lemma_Exp_Is_Monotonic (2, Element_Offset, 62);
          Lemma_Exp_Is_Monotonic (2, Left_Offset, 62);
 
          pragma Assert (2 ** Element_Offset <= 2 ** 62);
-         pragma Assert (2 ** Left_Offset <= 2 ** 62);
-         pragma Assert (2 ** ES <= Long_Integer'Last);
-
+         pragma Assert (2 ** Element_Type'Size <= Long_Integer'Last);
          pragma Assert (2 ** Left_Offset >= 0);
+         pragma Assert (2 ** Left_Offset <= Long_Integer'Last);
          pragma Assert ((Element_Type'Pos (D (I + 1)) mod 2 ** Element_Offset >= 0));
+
+         Lemma_Mult_Gt_0 (2 ** Left_Offset, (Element_Type'Pos (D (I + 1)) mod 2 ** Element_Offset));
          pragma Assert ((Element_Type'Pos (D (I + 1)) mod 2 ** Element_Offset * 2 ** Left_Offset >= 0));
 
-         pragma Assert (2 ** Element_Offset * 2 ** Left_Offset = 2 ** (Element_Offset + Left_Offset));
-         pragma Assert (2 ** (Element_Offset + Left_Offset) = 2 ** (Element_Offset + (Element_Size - Element_Offset)));
-         pragma Assert (Element_Offset + (Element_Size - Element_Offset) = Element_Size);
-         pragma Assert (2 ** (Element_Offset + (Element_Size - Element_Offset)) = 2 ** ES);
-         pragma Assert (2 ** Element_Offset * 2 ** Left_Offset = 2 ** ES);
-
-         pragma Assert (ES <= 62);
+         pragma Assert (2 ** Element_Offset * 2 ** (Element_Type'Size - Element_Offset)
+                        = 2 ** (Element_Offset + (Element_Type'Size - Element_Offset)));
+         pragma Assert (Element_Offset <= Element_Type'Size);
+         pragma Assert (2 ** (Element_Offset + (Element_Type_Size - Element_Offset)) = 2 ** Element_Type_Size);
+         pragma Assert (2 ** Element_Offset * 2 ** (Element_Type'Size - Element_Offset) <= 2 ** Element_Type_Size);
 
          Lemma_Lt_Mult_Lt_Lt (Left        => Element_Type'Pos (D (I + 1)) mod 2 ** Element_Offset,
                               Left_Bound  => 2 ** Element_Offset,
                               Right       => 2 ** Left_Offset,
                               Right_Bound => 2 ** Left_Offset,
-                              Result      => 2 ** ES);
-
-         pragma Assert ((Element_Type'Pos (D (I + 1)) mod 2 ** Element_Offset * 2 ** Left_Offset <= 2 ** ES));
-         pragma Assert ((Element_Type'Pos (D (I + 1)) mod 2 ** Element_Offset * 2 ** Left_Offset <= Long_Integer'Last));
-
+                              Result      => 2 ** Element_Type_Size);
          declare
-            Val : constant Long_Integer :=
-              (Element_Type'Pos (D (I)) / 2 ** Element_Offset
-               + (Element_Type'Pos (D (I + 1)) mod 2 ** Element_Offset * 2 ** Left_Offset)
-                  mod 2 ** Element_Size);
+            Right : constant Long_Integer := Element_Type'Pos (D (I)) / 2 ** Element_Offset;
+            Left  : constant Long_Integer := Element_Type'Pos (D (I + 1)) mod 2 ** Element_Offset * 2 ** Left_Offset;
+            pragma Assert (Right + Left <= Long_Integer'Last);
+            Val : constant Long_Integer := (Left + Right) mod 2 ** Element_Type'Size;
          begin
-            Result := Result + (2 ** (Element_Size * Natural (I)) * Val);
+            Lemma_Val_Lt_Mod (Element_Type'Pos (D (I)) / 2 ** Element_Offset, 2 ** Element_Type'Size);
+            pragma Assert (2 ** (Element_Type'Size * Natural (I)) * 2 ** Element_Type'Size <= Long_Integer'Last);
+            Lemma_Lt_Mult_Lt_Lt (Left        => 2 ** (Element_Type'Size * Natural (I)),
+                                 Left_Bound  => 2 ** (Element_Type'Size * Natural (I)),
+                                 Right       => Val,
+                                 Right_Bound => 2 ** Element_Type'Size,
+                                 Result      => Long_Integer'Last);
+
+            pragma Assert (2 ** (Element_Type_Size * Natural (I)) * Val <= Long_Integer'Last);
+            Result := Result + (2 ** (Element_Type_Size * Natural (I)) * Val);
          end;
       end loop;
 
-      Result := Result + 2 ** (Element_Size * Natural (Left)) * (Element_Type'Pos (D (Left)) / 2 ** Element_Offset);
+      Result := Result + 2 ** (Element_Type_Size * Natural (Left)) * (Element_Type'Pos (D (Left)) / 2 ** Element_Offset);
+      pragma Assert (Result <= Value_Type'Pos (Value_Type'Last));
+      pragma Assert (Result >= Value_Type'Pos (Value_Type'First));
       return Value_Type'Val (Result);
    end Extract;
 
--- tests/tests.adb
+++ tests/tests.adb
@@ -47,19 +47,19 @@ is
               Error     => "Invalid result");
    end Check;
 
-   procedure Check_U57 is new Check (U57);
+--     procedure Check_U57 is new Check (U57);
    procedure Check_U13 is new Check (U13);
-   procedure Check_U7  is new Check (U7);
-   procedure Check_U2  is new Check (U2);
-   procedure Check_U1  is new Check (U1);
-   procedure Check_I13 is new Check (I13);
-   procedure Check_I7  is new Check (I7);
-   procedure Check_I57 is new Check (I57);
-   procedure Check_I2  is new Check (I2);
-   procedure Check_I1  is new Check (I1);
+--     procedure Check_U7  is new Check (U7);
+--     procedure Check_U2  is new Check (U2);
+--     procedure Check_U1  is new Check (U1);
+--     procedure Check_I13 is new Check (I13);
+--     procedure Check_I7  is new Check (I7);
+--     procedure Check_I57 is new Check (I57);
+--     procedure Check_I2  is new Check (I2);
+--     procedure Check_I1  is new Check (I1);
 
    Data   : Byte_Array (1..3) := (16#de#, 16#ad#, 16#be#);
-   Data2  : Byte_Array (1..2) := (16#ff#, 16#ff#);
+   Data2  : Byte_Array (1 .. 2) := (16#ff#, 16#ff#);
    Data64 : Byte_Array (1..8) := (16#de#, 16#ad#, 16#be#, 16#ef#, 16#ca#, 16#fe#, 16#ba#, 16#be#);
 
 begin
@@ -71,67 +71,67 @@ begin
    Check_U13 ("Extract U13, 3 bytes, Off 3", Data, 3, 16#15b7#);
    Check_U13 ("Extract U13, 3 bytes, Off 7", Data, 7, 16#1d5b#);
 
-   Check_U7 ("Extract U7, 3 bytes, Off 0", Data, 0, 16#3e#);
-   Check_U7 ("Extract U7, 3 bytes, Off 1", Data, 1, 16#5f#);
-   Check_U7 ("Extract U7, 3 bytes, Off 2", Data, 2, 16#6f#);
-   Check_U7 ("Extract U7, 3 bytes, Off 7", Data, 7, 16#5b#);
-
-   Check_U57 ("Extract U57, 8 bytes, Off 0", Data64, 0, 16#adbeefcafebabe#);
-   Check_U57 ("Extract U57, 8 bytes, Off 3", Data64, 3, 16#1d5b7ddf95fd757#);
-   Check_U57 ("Extract U57, 8 bytes, Off 7", Data64, 7, 16#1bd5b7ddf95fd75#);
-
-   Check_U2 ("Extract U2, 3 bytes, Off 23", Data (1..1), 0, 2);
-   Check_U2 ("Extract U2, 3 bytes, Off 22", Data (1..1), 6, 3);
-   Check_U2 ("Extract U2, 3 bytes, Off 16", Data (1..2), 7, 1);
-   Check_U2 ("Extract U2, 3 bytes, Off 17", Data (3..3), 0, 2);
-   Check_U2 ("Extract U2, 3 bytes, Off 18", Data (3..3), 1, 3);
-   Check_U2 ("Extract U2, 3 bytes, Off  1", Data (3..3), 1, 3);
-   Check_U2 ("Extract U2, 3 bytes, Off  0", Data (3..3), 0, 2);
-   Check_U2 ("Extract U2, 3 bytes, Off 16", Data2, 7, 3);
-
-   Check_U1 ("Extract U1, 3 bytes, Off 23", Data (3..3), 7, 1);
-   Check_U1 ("Extract U1, 3 bytes, Off 17", Data (3..3), 1, 1);
-   Check_U1 ("Extract U1, 3 bytes, Off 16", Data (3..3), 0, 0);
-   Check_U1 ("Extract U1, 3 bytes, Off 15", Data (2..2), 7, 1);
-   Check_U1 ("Extract U1, 3 bytes, Off 14", Data (2..2), 6, 0);
-   Check_U1 ("Extract U1, 3 bytes, Off 13", Data (2..2), 5, 1);
-   Check_U1 ("Extract U1, 3 bytes, Off 12", Data (2..2), 4, 0);
-   Check_U1 ("Extract U1, 3 bytes, Off 11", Data (2..2), 3, 1);
-   Check_U1 ("Extract U1, 3 bytes, Off  1", Data (1..1), 1, 1);
-   Check_U1 ("Extract U1, 3 bytes, Off  0", Data (1..1), 0, 0);
-
-   Check_I13 ("Extract I13, 3 bytes, Off 0", Data, 0, 16#0dbe#);
-   Check_I13 ("Extract I13, 3 bytes, Off 1", Data, 1, 16#16df#);
-   Check_I13 ("Extract I13, 3 bytes, Off 2", Data, 2, 16#0b6f#);
-   Check_I13 ("Extract I13, 3 bytes, Off 3", Data, 3, 16#15b7#);
-   Check_I13 ("Extract I13, 3 bytes, Off 7", Data, 7, 16#1d5b#);
-
-   Check_I7 ("Extract I7, 3 bytes, Off 0", Data, 0, 16#3e#);
-   Check_I7 ("Extract I7, 3 bytes, Off 1", Data, 1, 16#5f#);
-   Check_I7 ("Extract I7, 3 bytes, Off 2", Data, 2, 16#6f#);
-   Check_I7 ("Extract I7, 3 bytes, Off 7", Data, 7, 16#5b#);
-
-   Check_I57 ("Extract I57, 8 bytes, Off 0", Data64, 0, 16#adbeefcafebabe#);
-   Check_I57 ("Extract I57, 8 bytes, Off 3", Data64, 3, 16#1d5b7ddf95fd757#);
-   Check_I57 ("Extract I57, 8 bytes, Off 7", Data64, 7, 16#1bd5b7ddf95fd75#);
-
-   Check_I2 ("Extract I2, 3 bytes, Off 23", Data (1..1), 0, 2);
-   Check_I2 ("Extract I2, 3 bytes, Off 22", Data (1..1), 6, 3);
-   Check_I2 ("Extract I2, 3 bytes, Off 16", Data (1..2), 7, 1);
-   Check_I2 ("Extract I2, 3 bytes, Off 17", Data (3..3), 0, 2);
-   Check_I2 ("Extract I2, 3 bytes, Off 18", Data (3..3), 1, 3);
-   Check_I2 ("Extract I2, 3 bytes, Off  1", Data (3..3), 1, 3);
-   Check_I2 ("Extract I2, 3 bytes, Off  0", Data (3..3), 0, 2);
-
-   Check_I1 ("Extract I1, 3 bytes, Off 23", Data (3..3), 7, 1);
-   Check_I1 ("Extract I1, 3 bytes, Off 17", Data (3..3), 1, 1);
-   Check_I1 ("Extract I1, 3 bytes, Off 16", Data (3..3), 0, 0);
-   Check_I1 ("Extract I1, 3 bytes, Off 15", Data (2..2), 7, 1);
-   Check_I1 ("Extract I1, 3 bytes, Off 14", Data (2..2), 6, 0);
-   Check_I1 ("Extract I1, 3 bytes, Off 13", Data (2..2), 5, 1);
-   Check_I1 ("Extract I1, 3 bytes, Off 12", Data (2..2), 4, 0);
-   Check_I1 ("Extract I1, 3 bytes, Off 11", Data (2..2), 3, 1);
-   Check_I1 ("Extract I1, 3 bytes, Off  1", Data (1..1), 1, 1);
-   Check_I1 ("Extract I1, 3 bytes, Off  0", Data (1..1), 0, 0);
+--   Check_U7 ("Extract U7, 3 bytes, Off 0", Data, 0, 16#3e#);
+--   Check_U7 ("Extract U7, 3 bytes, Off 1", Data, 1, 16#5f#);
+--   Check_U7 ("Extract U7, 3 bytes, Off 2", Data, 2, 16#6f#);
+--   Check_U7 ("Extract U7, 3 bytes, Off 7", Data, 7, 16#5b#);
+--
+--   Check_U57 ("Extract U57, 8 bytes, Off 0", Data64, 0, 16#adbeefcafebabe#);
+--   Check_U57 ("Extract U57, 8 bytes, Off 3", Data64, 3, 16#1d5b7ddf95fd757#);
+--   Check_U57 ("Extract U57, 8 bytes, Off 7", Data64, 7, 16#1bd5b7ddf95fd75#);
+--
+--   Check_U2 ("Extract U2, 3 bytes, Off 23", Data (1..1), 0, 2);
+--   Check_U2 ("Extract U2, 3 bytes, Off 22", Data (1..1), 6, 3);
+--   Check_U2 ("Extract U2, 3 bytes, Off 16", Data (1..2), 7, 1);
+--   Check_U2 ("Extract U2, 3 bytes, Off 17", Data (3..3), 0, 2);
+--   Check_U2 ("Extract U2, 3 bytes, Off 18", Data (3..3), 1, 3);
+--   Check_U2 ("Extract U2, 3 bytes, Off  1", Data (3..3), 1, 3);
+--   Check_U2 ("Extract U2, 3 bytes, Off  0", Data (3..3), 0, 2);
+--   Check_U2 ("Extract U2, 3 bytes, Off 16", Data2, 7, 3);
+--
+--   Check_U1 ("Extract U1, 3 bytes, Off 23", Data (3..3), 7, 1);
+--   Check_U1 ("Extract U1, 3 bytes, Off 17", Data (3..3), 1, 1);
+--   Check_U1 ("Extract U1, 3 bytes, Off 16", Data (3..3), 0, 0);
+--   Check_U1 ("Extract U1, 3 bytes, Off 15", Data (2..2), 7, 1);
+--   Check_U1 ("Extract U1, 3 bytes, Off 14", Data (2..2), 6, 0);
+--   Check_U1 ("Extract U1, 3 bytes, Off 13", Data (2..2), 5, 1);
+--   Check_U1 ("Extract U1, 3 bytes, Off 12", Data (2..2), 4, 0);
+--   Check_U1 ("Extract U1, 3 bytes, Off 11", Data (2..2), 3, 1);
+--   Check_U1 ("Extract U1, 3 bytes, Off  1", Data (1..1), 1, 1);
+--   Check_U1 ("Extract U1, 3 bytes, Off  0", Data (1..1), 0, 0);
+--
+--   Check_I13 ("Extract I13, 3 bytes, Off 0", Data, 0, 16#0dbe#);
+--   Check_I13 ("Extract I13, 3 bytes, Off 1", Data, 1, 16#16df#);
+--   Check_I13 ("Extract I13, 3 bytes, Off 2", Data, 2, 16#0b6f#);
+--   Check_I13 ("Extract I13, 3 bytes, Off 3", Data, 3, 16#15b7#);
+--   Check_I13 ("Extract I13, 3 bytes, Off 7", Data, 7, 16#1d5b#);
+--
+--   Check_I7 ("Extract I7, 3 bytes, Off 0", Data, 0, 16#3e#);
+--   Check_I7 ("Extract I7, 3 bytes, Off 1", Data, 1, 16#5f#);
+--   Check_I7 ("Extract I7, 3 bytes, Off 2", Data, 2, 16#6f#);
+--   Check_I7 ("Extract I7, 3 bytes, Off 7", Data, 7, 16#5b#);
+--
+--   Check_I57 ("Extract I57, 8 bytes, Off 0", Data64, 0, 16#adbeefcafebabe#);
+--   Check_I57 ("Extract I57, 8 bytes, Off 3", Data64, 3, 16#1d5b7ddf95fd757#);
+--   Check_I57 ("Extract I57, 8 bytes, Off 7", Data64, 7, 16#1bd5b7ddf95fd75#);
+--
+--   Check_I2 ("Extract I2, 3 bytes, Off 23", Data (1..1), 0, 2);
+--   Check_I2 ("Extract I2, 3 bytes, Off 22", Data (1..1), 6, 3);
+--   Check_I2 ("Extract I2, 3 bytes, Off 16", Data (1..2), 7, 1);
+--   Check_I2 ("Extract I2, 3 bytes, Off 17", Data (3..3), 0, 2);
+--   Check_I2 ("Extract I2, 3 bytes, Off 18", Data (3..3), 1, 3);
+--   Check_I2 ("Extract I2, 3 bytes, Off  1", Data (3..3), 1, 3);
+--   Check_I2 ("Extract I2, 3 bytes, Off  0", Data (3..3), 0, 2);
+--
+--   Check_I1 ("Extract I1, 3 bytes, Off 23", Data (3..3), 7, 1);
+--   Check_I1 ("Extract I1, 3 bytes, Off 17", Data (3..3), 1, 1);
+--   Check_I1 ("Extract I1, 3 bytes, Off 16", Data (3..3), 0, 0);
+--   Check_I1 ("Extract I1, 3 bytes, Off 15", Data (2..2), 7, 1);
+--   Check_I1 ("Extract I1, 3 bytes, Off 14", Data (2..2), 6, 0);
+--   Check_I1 ("Extract I1, 3 bytes, Off 13", Data (2..2), 5, 1);
+--   Check_I1 ("Extract I1, 3 bytes, Off 12", Data (2..2), 4, 0);
+--   Check_I1 ("Extract I1, 3 bytes, Off 11", Data (2..2), 3, 1);
+--   Check_I1 ("Extract I1, 3 bytes, Off  1", Data (1..1), 1, 1);
+--   Check_I1 ("Extract I1, 3 bytes, Off  0", Data (1..1), 0, 0);
 
 end Tests;
