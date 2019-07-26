381c3c038dfc74e70043daed85ad9e13e7131db1
Alexander Senier <senier@componolit.com>
2019-07-26 14:06:38 +0200

    WIP

--- src/extracts.adb
+++ src/extracts.adb
@@ -7,14 +7,18 @@ is
    function Extract (Data   : Array_Type;
                      Offset : Offset_Type) return Value_Type
    is
-      Least_Significant_Index : constant Long_Integer :=
-         Offset_Type'Pos (Offset) / Long_Integer (Element_Type'Size);
+      --  Index pointing to least significant element
+      Least_Significant_Index : constant Long_Integer := Offset_Type'Pos (Offset) / Long_Integer (Element_Type'Size);
+
+      --  Bits the least significant element (LSE) is shifted left relative to a single element
+      LSE_Offset : constant Natural := Offset_Type'Pos (Offset) mod Element_Type'Size;
+
+      --  Index pointing to most significant index
       Most_Significant_Index  : constant Long_Integer :=
          (Offset_Type'Pos (Offset) + Long_Integer (Value_Type'Size) - 1) / Long_Integer (Element_Type'Size);
 
-      Element_Offset : constant Natural      := Offset_Type'Pos (Offset) mod Element_Type'Size;
-      Left_Offset    : constant Natural      := Element_Type'Size - Element_Offset;
-      Result         : Long_Integer          := 0;
+      --  Bits the most significant element (MSE) is shifted right relative to a single element
+      MSE_Offset : constant Natural := Element_Type'Size - LSE_Offset;
 
       function Element_Type_Size return Natural is (Element_Type'Size)
         with Post => (Element_Type_Size'Result = Element_Type'Size);
@@ -24,7 +28,7 @@ is
 
       function D (Pos : Long_Integer) return Element_Type
       is
-         E : constant Natural := (Element_Offset + Value_Type'Size + Element_Type'Size - 1) mod Element_Type'Size + 1;
+         E : constant Natural := (LSE_Offset + Value_Type'Size + Element_Type'Size - 1) mod Element_Type'Size + 1;
       begin
          declare
             Mask : constant Long_Integer := (if Pos < Most_Significant_Index then 2 ** Element_Type'Size else 2 ** E);
@@ -34,19 +38,20 @@ is
          end;
       end D;
 
+      Result : Long_Integer := 0;
    begin
       for I in Least_Significant_Index .. Most_Significant_Index - 1
       loop
          declare
-            Current : constant Long_Integer := Element_Type'Pos (D (I)) / 2 ** Element_Offset;
-            Next    : constant Long_Integer := Element_Type'Pos (D (I + 1)) mod 2 ** Element_Offset * 2 ** Left_Offset;
+            Current : constant Long_Integer := Element_Type'Pos (D (I)) / 2 ** LSE_Offset;
+            Next    : constant Long_Integer := Element_Type'Pos (D (I + 1)) mod 2 ** LSE_Offset * 2 ** MSE_Offset;
             Value   : constant Long_Integer := (Current + Next) mod 2 ** Element_Type'Size;
          begin
             Result := Result + (2 ** (Element_Type_Size * Natural (I)) * Value);
          end;
       end loop;
       Result := Result + 2 ** (Element_Type_Size * Natural (Most_Significant_Index))
-                           * (Element_Type'Pos (D (Most_Significant_Index)) / 2 ** Element_Offset);
+                           * (Element_Type'Pos (D (Most_Significant_Index)) / 2 ** LSE_Offset);
       return Value_Type'Val (Result);
    end Extract;
 
